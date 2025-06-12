1e78fb901f96f3675633d7ed5d7e741bd3005cd0
fabiomarques1 <94563590+fabiomarques1@users.noreply.github.com>
2025-06-12 16:54:46 -0300

    Add files via upload

--- Loja/src/dao/FreteDAO.java
+++ Loja/src/dao/FreteDAO.java
@@ -1,8 +1,10 @@
 
 package dao;
 
+import java.util.List;
 import modelo.Frete;
 
 public interface FreteDAO {
     public int inserir(Frete frete);
+    public List<Frete>listar();
 }
--- Loja/src/dao/FreteDAOJDBC.java
+++ Loja/src/dao/FreteDAOJDBC.java
@@ -1,5 +1,9 @@
 package dao;
 
+import static dao.DAOGenerico.executarConsulta;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import modelo.Frete;
 
 public class FreteDAOJDBC extends DAOGenerico implements FreteDAO {
@@ -8,14 +12,13 @@ public class FreteDAOJDBC extends DAOGenerico implements FreteDAO {
     public int inserir(Frete frete) {
         StringBuilder sqlBuilder = new StringBuilder();
         sqlBuilder
-                .append("insert into frete(codigo, tipo, valor) ")
-                .append("VALUES (?, ?, ?)");
+                .append("insert into frete(tipo, valor) ")
+                .append("VALUES (?, ?)");
      
         String insert = sqlBuilder.toString();
         int linha = 0;
         try {
-            linha = executarComando(insert, frete.getCodigo(),
-                                            frete.getTipo(),
+            linha = executarComando(insert, frete.getTipo(),
                                             frete.getValor()
             );
         } catch (Exception e) {
@@ -23,6 +26,26 @@ public class FreteDAOJDBC extends DAOGenerico implements FreteDAO {
         }
  
         return linha;
+    }
+    
+    @Override
+    public List<Frete> listar() {
+        ResultSet rset;
+        String select = "SELECT * FROM frete";
+        List<Frete> fretes = new ArrayList<>();
+        try {        
+            rset = executarConsulta(select);
+            while (rset.next()) {
+                Frete frete = new Frete();
+                frete.setCodigo(rset.getInt("codigo"));
+                frete.setTipo(rset.getString("tipo"));
+                frete.setValor(rset.getFloat("valor"));
+                fretes.add(frete);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+        return fretes;
     }
     
 }
